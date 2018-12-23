1fb0a393c500820576aa646957f03f77c84717a1
Dylan Lott <lott.dylan@gmail.com>
2018-12-22 20:36:07 -0700

    adds layout and styles to uploader page

--- parser/app.py
+++ parser/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,10 +8,6 @@ def hello():
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        f = request.files['replay.mpq']
         return 'UPLOAD'
     else:
-        return 'RETURN UPLOAD PAGE'
-        # f = request.files['the_file']
-        # f.save('/var/www/uploads/uploaded_file.txt')
-
+        return render_template('uploader.html')
